63b02b40ab1bbb67668b5bb1d3aa65ad3af709ab
allenjj98 <143020153+allenjj98@users.noreply.github.com>
2024-03-13 16:55:40 +0000

    Create and style square div

---
+++ sketch.js
@@ -0,0 +1,21 @@
+// create variables called vertical and horizonal to store figures for both of these orientations. 
+const vertical = 16;
+const horizontal = 16;
+
+// select #grid div and store in a variable called grid
+const grid = document.querySelector('#grid');
+
+// create a box that later make up part of the grid
+let div = document.createElement("div");
+div.classList.add("box");
+
+
+// Add a new div and adjust style. Do this until the number of divs surpasses the number stored in the horizontal and vertical variables.
+
+
+
+
+
+
+
+// adjust the allignment of the divs that they are spread across the screen. 
