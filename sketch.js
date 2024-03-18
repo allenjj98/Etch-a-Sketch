f80cb483a0cdb178baa7a32aa3b2941561b46678
allenjj98 <143020153+allenjj98@users.noreply.github.com>
2024-03-18 08:13:15 +0000

    Add ability to generate new grid

--- sketch.js
+++ sketch.js
@@ -3,47 +3,49 @@ const grid = document.querySelector('#grid');
 
 
 // create variables called vertical and horizonal to store figures for both of these orientations. 
-const vertical = 16;
-const horizontal = 16;
+const dimension = 16;
 
 
 // create a button variable that will represent the button in the dom. Attach an eventlistener to the button that will use prompt to display a pop-up to the user. 
 const button = document.querySelector('#btn');
 button.addEventListener('click', () => {
-    let gridWidth = parseInt(prompt("Width of new box: "));
-    let gridHeight = parseInt(prompt("Height of new box: "));
+    let gridSize = parseInt(prompt("Grid Size: "));
     while (grid.hasChildNodes()) {
         grid.removeChild(grid.firstChild)
     }
+    createGrid(gridSize)
+    hover()
 })
 
 
 
 
-
-
-
-
-
-// create a box that later make up part of the grid
-let div; 
-
-
-// Add a new div and adjust style. Do this until the number of divs surpasses the number stored in the horizontal and vertical variables.
-for (let i = 0; i < vertical; i++) {
-    for (let j = 0; j < horizontal; j++) {
-        div = document.createElement("div");
-        div.classList.add("box");
-        grid.appendChild(div);
+function createGrid(size) {
+    let div;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            div = document.createElement("div");
+            div.classList.add("box");
+            let width = 100 / size;
+            console.log(width)
+            div.style.width = width + "%";
+            grid.appendChild(div);
+        }
     }
 }
 
-const boxes = document.querySelectorAll(".box");
 
-boxes.forEach((box) => {
-    box.addEventListener("mouseenter", () => {
-        box.classList.add("new");
+function hover() {
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+            box.classList.add("new");
+        });
     });
-});
+}
 
+
+createGrid(dimension)
+hover()
 
